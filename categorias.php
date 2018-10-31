139c30360ecde6a4eeba2c4f39800d55468eaeb8
Hernan Moran <hernmoran@gmail.com>
2018-10-31 02:51:13 -0300

    first commit

---
+++ categorias.php
@@ -0,0 +1,52 @@
+
+
+<section class="categorias">
+
+    <div class="center">
+        <img src="images/cuerno.png">
+        <p>Lampara Cuerno</p>
+    </div>
+    <div class="center">
+        <img src="images/huevo.png">
+        <p>Lampara Eggs</p>
+    </div>
+    <div class="center">
+        <img src="images/pinza.png">
+        <p>Lampara Pinza</p>
+    </div> 
+  
+</section>
+
+<section class="griduno">
+        <div class="grid-itemtitulo">
+            <h5><hr />LINEA ILUMINACION 1018<hr /></h5>
+            </div>
+</section>
+
+<section class="grid">
+
+        <article class="grid-item">
+        <img class="centrado" src="images/ovo.png" alt="">
+            <p>Colgantes</p>
+            <button class="centrado" type="button" class="btn btn-outline-secondary">VER MAS</button>
+        </article>
+
+        <article class="grid-item">
+        <img class="centrado" src="images/ovo.png" alt="">
+            <p>De Mesa</p>
+            <button class="centrado" type="button" class="btn btn-outline-secondary">VER MAS</button>
+        </article>
+
+        <article class="grid-item">
+        <img class="centrado" src="images/ovo.png" alt="">
+            <p>De Pie</p>
+            <button class="centrado" type="button" class="btn btn-outline-secondary">VER MAS</button>
+        </article>
+
+        <article class="grid-item">
+        <img class="centrado" src="images/ovo.png" alt="">
+            <p>Apliques</p>
+            <button class="centrado"type="button" class="btn btn-outline-secondary">VER MAS</button>
+        </article>
+
+</section>
---
+++ home.php
@@ -0,0 +1,40 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/bombillas.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+    <p class="titulos">ILUMINACION</p>
+    <p class="subtexto">Espacios iluminados</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/conicas.png" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+    <p class="titulos">COLORES</p>
+    <p class="subtexto">La iluminacion como centro decorativo</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/lamprayas.png" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <p class="titulos">GENROS</p>
+    <p class="subtexto">Telas y texturas refinadas</p>
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
